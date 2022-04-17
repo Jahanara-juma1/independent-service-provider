d79f4973dd381a33c0d3c21e49d29d26c8506fdf
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-04-17 15:05:34 +0600

    services addad

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
